71593d21165b118ff04e425c9999eba913d59c09
moastafa <moastfa9090@gmail.com>
2023-08-25 10:49:04 +0300

    new update

--- INTRO-python/sessions/intro.py
+++ INTRO-python/sessions/intro.py
@@ -3,14 +3,5 @@
 # using naive method
  
 # initializing list
-test_list = [1, 4, 5, 6, 7]
- 
-# Printing list
-print("Original list is : " + str(test_list))
- 
-# using naive method to
-# get index and value
-print("List index-value are : ")
-for i in range(len(test_list)):
-    print(i, end=" ")
-    print(test_list[i])
+# modifing in file lets see on git 
+print ( 'hello git ')
