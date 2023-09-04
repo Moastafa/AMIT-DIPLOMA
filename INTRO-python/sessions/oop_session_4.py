10608414bf3efc9f696fb56298939ad39f618f63
moastafa <moastfa9090@gmail.com>
2023-09-04 22:39:19 +0300

    add new folder

---
+++ INTRO-python/sessions/oop_session_4.py
@@ -0,0 +1,35 @@
+class Dog :
+    def __init__(self,Dog_name,Dog_age): # the parameters 
+        self.name=Dog_name
+        self.age=Dog_age
+        
+rayen= Dog ( 'rayen',12)
+        
+print=( rayen.age)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
